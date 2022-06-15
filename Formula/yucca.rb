1226fe04139d3da9be03a91f956e371da05fee23
Eugeniy Medvedev <jidckii@gmail.com>
2022-06-15 09:35:54 +0200

    bump 0.7.0

--- Formula/yucca.rb
+++ Formula/yucca.rb
@@ -1,23 +1,23 @@
 class Yucca < Formula
   desc "Yucca Stream Server"
   homepage "http://yucca.app/"
-  version "0.6.0"
+  version "0.7.0"
 
   depends_on "ffmpeg" => :recommended
   depends_on "docker" => :recommended
 
   if OS.mac?
-    url "https://get.yucca.app/v0.6.0/darwin_amd64/yucca"
-    sha256 "a5d6810d7957375781562ff174053ca4a824739320117797d03184057896961a"
+    url "https://releases.yucca.app/v0.7.0/darwin_amd64/free/yucca"
+    sha256 "68147ed2304e2c42dce73b46dbad4f129f943e81737cb069b3eb5d0d0417f24f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://get.yucca.app/v0.6.0/linux_amd64/yucca"
-      sha256 "d89672ff42ebd601fd5c684c12e9e7810c3b31f5edf692faa9e0ed9f66ae4487"
+      url "https://releases.yucca.app/v0.7.0/linux_amd64/free/yucca"
+      sha256 "134f32a279249bc6bea3e9d39ec1c713836ca12d198ddbdc9938cc5c03690a72"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://get.yucca.app/v0.6.0/linux_arm64/yucca"
-        sha256 "3cbdfde6476e1938bcc3ecf15116bb54e0329372ad51eacef3d02ceabafe8bc7"
+        url "https://releases.yucca.app/v0.7.0/linux_arm64/free/yucca"
+        sha256 "635f4ef14b87576a971d038a5fd808812162b3e5be612c1582a9044444d7d920"
       else
       end
     end
